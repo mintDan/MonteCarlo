87b052aff9045225f90e53c33321f8d4a8e25234
Dan <dankroga@gmail.com>
2017-01-09 22:31:36 +0100

    changed MonteCarlo.py to be module

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,53 +1,70 @@
+"""
+Using a simple monte carlo method to calculate a definite integral
+modified 2017
+Dan Krog 
+"""
 import numpy as np
 import matplotlib as mpl
-mpl.use('Agg')
+#mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-a = 0
-b = 10
-n = 500
-x = np.linspace(a,b,n)
-#fplot = np.array(x**2)
 
-def f(x):
-	return x**2
+def MC(a,b,n):
 
+	#Making array for plotting, doesn't have to be set with n.
+	x = np.linspace(a,b,n)
 
-xg = np.random.uniform(a, b, size=n)
-yg = np.random.uniform(a, b**2, size=n)
+	xg = np.random.uniform(a, b, size=n)
+	yg = np.random.uniform(a, b**2, size=n)
 
-inse = np.array([],dtype=bool)
-below = 0
-for i in range(n):
-	if yg[i] <= f(xg[i]):
-		below+=1
-		inse = np.append(inse,i)
+	inse = np.array([],dtype=bool)
+	below = 0
+	for i in range(n):
+		if yg[i] <= f(xg[i]):
+			below+=1
+			inse = np.append(inse,i)
 
-#int x^2 -> (1/3)*x^3
-#[(1/3)x^3]^1,0 = 1/3
-P = below/n
-print('Probability')
-print(P)
+	#int x^2 -> (1/3)*x^3
+	#[(1/3)x^3]^1,0 = 1/3
+	P = below/n
+	print('Probability to land under function')
+	print(P)
 
 
 
-Area = P*(b-a)*f(x[-1])
-print('Approximated area')
-print(Area)
+	Area = P*(b-a)*np.max(f(x))
+	#print('Approximated area')
+	#print(Area)
 
-print('Analytical area')
-AArea = (1/3)*10**3
-print(AArea)
+	#print('Analytical area')
+	AArea = (1/3)*10**3
+	#print(AArea)
+
+	plot(x,f(x),xg,yg,xg[inse],yg[inse],AArea,Area)
 
 
-plt.plot(x,f(x))
-#pyplot.hold(True)
-#pyplot.plot(x,f(x),c='black')
-#pyplot.scatter(xg,yg,color='blue')
-#pyplot.scatter(xg[inse],yg[inse],color='black')
-#pyplot.xlabel('x')
-#pyplot.ylabel('y')
-#pyplot.title('Integrate $x^2$, area = {0:2f}'.format(AArea))
-#pyplot.legend(['Approx Area = {0:2f}'.format(Area)])
-#pyplot.savefig('MCint.png', bbox_inches='tight')
-plt.show()
+def plot(x,fx,xg,yg,xginse,yginse,AArea,Area):
+	plt.hold(True)
+	plt.plot(x,fx,color='black')
+	plt.scatter(xg,yg,color='blue')
+	plt.scatter(xginse,yginse,color='black')
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.title('Integrate $x^2$, area = {0:2f}'.format(AArea))
+	plt.legend(['Approx Area = {0:2f}'.format(Area)])
+	plt.savefig('MCint.png', bbox_inches='tight')
+	plt.show()
+
+
+
+if __name__ == "__main__":
+
+	x0 = 0 #left boundary
+	x1 = 10 #right boundary
+	n = 500 #number of dots/random points
+
+	#function to be integrated
+	def f(x):
+		return x**2
+
+	MC(x0,x1,n)
